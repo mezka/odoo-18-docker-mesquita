ca165c835a973d3c5c392962a90c359614edc620
Emiliano Mesquita <emiliano_mesquita@hotmail.com>
2024-11-11 14:40:07 -0300

    Experimental (module): load data via csv

---
+++ addons/mesquita_demo_csv/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'mesquita_demo_csv',
+    'version': '0.1',
+    'depends': ['product'],
+    'demo': [
+        'data/product.attribute.csv',
+        'data/product.attribute.value.csv',
+        'data/product.template.csv',
+        'data/product.template.attribute.line.csv'
+    ],
+}
